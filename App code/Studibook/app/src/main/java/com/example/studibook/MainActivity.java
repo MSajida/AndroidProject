7978a9693f46dc47b3e1bf79071d2828ab62b193
Sreeja Kumbhum <98132897+ksreejareddy@users.noreply.github.com>
2022-10-08 23:45:33 -0500

    Main activity piechart commit

--- App code/Studibook/app/src/main/java/com/example/studibook/MainActivity.java
+++ App code/Studibook/app/src/main/java/com/example/studibook/MainActivity.java
@@ -1,14 +1,73 @@
 package com.example.studibook;
 
+// Import the required libraries
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity
+        extends AppCompatActivity {
+
+    // Create the object of TextView
+    // and PieChart class
+    TextView food, health, travel, miscellaneous;
+    PieChart pieChart;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Link those objects with their
+        // respective id's that
+        // we have given in .XML file
+        food = findViewById(R.id.food);
+        health = findViewById(R.id.health);
+        travel = findViewById(R.id.travel);
+        miscellaneous = findViewById(R.id. miscellaneous);
+        pieChart = findViewById(R.id.piechart);
+
+        // Creating a method setData()
+        // to set the text in text view and pie chart
+        setData();
+    }
+
+    private void setData()
+    {
+
+        // Set the percentage of language used
+        food.setText(Integer.toString(40));
+        health.setText(Integer.toString(30));
+        travel.setText(Integer.toString(5));
+        miscellaneous.setText(Integer.toString(25));
+
+        // Set the data and color to the pie chart
+        pieChart.addPieSlice(
+                new PieModel(
+                        "R",
+                        Integer.parseInt(food.getText().toString()),
+                        Color.parseColor("#FFA726")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "health",
+                        Integer.parseInt(health.getText().toString()),
+                        Color.parseColor("#66BB6A")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "C++",
+                        Integer.parseInt(travel.getText().toString()),
+                        Color.parseColor("#EF5350")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        " miscellaneous ",
+                        Integer.parseInt(miscellaneous.getText().toString()),
+                        Color.parseColor("#29B6F6")));
+
+        // To animate the pie chart
+        pieChart.startAnimation();
     }
 }
