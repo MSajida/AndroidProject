45edf04e1bf51b085921e14684ccc7670e8ea6b9
ThaaraniY <98137201+ThaaraniY@users.noreply.github.com>
2022-10-20 12:27:11 -0500

    created add member model

---
+++ App code/Studibook/app/src/main/java/com/example/studibook/AddmemberModel.java
@@ -0,0 +1,40 @@
+package com.example.studibook;
+
+public class AddmemberModel {
+    String name;
+    String sid;
+    String mail;
+
+    public AddmemberModel() {
+    }
+
+    public AddmemberModel(String name, String sid, String mail) {
+        this.name = name;
+        this.sid = sid;
+        this.mail = mail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
